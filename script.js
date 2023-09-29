fb341d36aad660ff8082291e810a7afa91f37a43
manojkrishnan1 <127621202+manojkrishnan1@users.noreply.github.com>
2023-09-29 17:55:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function convertToRoman(num) {
-  let roman = ['M', 'XM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
+  let roman = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
   let values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]; 
         let i = 0;
         let arr = [];
